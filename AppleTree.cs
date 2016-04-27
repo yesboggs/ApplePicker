c40f30e10b33d7610eb20d0be4e14ffc271dda15
Chris Boggs <yesboggs@aol.com>
2016-04-27 15:04:44 -0400

    Added files via upload

---
+++ AppleTree.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirection = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Use this for initialization
+    void Start () {
+        //drop apples every second
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);//goes right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //goes left
+        }
+       
+	}
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1;
+            //randomly change directions
+        }
+    }
+}
---
+++ HighScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+    static public int score = 1000;
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GUIText gt = this.GetComponent<GUIText>();
+        gt.text = "High Score: " + score;
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+	}
+}
